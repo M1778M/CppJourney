b9662b06eb77cb75120ae83802bd3b5fa18f2221
M1778M <m1778.pc@gmail.com>
2025-02-25 23:43:25 +0330

    Fixed it lol

--- 3-bubble-sort.cpp
+++ 3-bubble-sort.cpp
@@ -31,6 +31,18 @@ void m1sort(int arr[], size_t size){
 	}
 }
 
+void bubblesort(int arr[], size_t size){
+	int temp;
+	for(int i = 0; i < size - 1; i++){
+		for (int j = i; j < size - 1; j++)
+			if (arr[i] > arr[j]){
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+	}
+}
+
 
 void print_array(int arr[], size_t size){
 	for (int i = 0; i< size; i++){
@@ -40,21 +52,20 @@ void print_array(int arr[], size_t size){
 
 int main(){
 	srand(time(NULL));
-	int ar[10];
-	for (int i = 0; i < 10; i++)
+	int ar[10000];// = {3,5,7,2,1,3,7,9};
+	for (int i = 0; i < 10000; i++)
 		ar[i] = rand();
 
-	for(int e: ar){
-		std::cout << e << ' ';
-	}
-	std::cout << "Is sorted: " << is_sorted(ar, sizeof(ar)/sizeof(ar[0])) <<'\n';
-
+	// for(int e: ar){
+	// 	std::cout << e << ' ';
+	// }
+	std::cout << '\n';
 	
-	m1sort(ar,sizeof(ar)/sizeof(ar[0]));
-
-	
-	for(int e: ar){
-		std::cout << e << ' ';
-	}
+	// m1sort(ar,sizeof(ar)/sizeof(ar[0]));
+	bubblesort(ar, sizeof(ar)/sizeof(ar[0]));
+ 
+	// for(int e: ar){
+	// 	std::cout << e << ' ';
+	// }
 	std::cout << '\n';
 }
